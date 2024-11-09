7b63d169fcaec7f5963e84716e3d024a600e21ce
Yosef-86 <144876092+Yosef-86@users.noreply.github.com>
2024-11-09 23:47:09 +0800

    Delete midterm directory

--- midterm/classes/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = 'localhost';
-$dbname = 'online_ordering_system';
-$user = 'root';
-$pass = '';
-$connection = new PDO("mysql:host=$host;dbname=$dbname;charset=UTF8",$user,$pass);
-?>
